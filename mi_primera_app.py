fdad11780b383591ca89eb05506243ef985defae
SantiagoMejia98 <141966190+SantiagoMejia98@users.noreply.github.com>
2023-08-28 16:23:53 -0500

    Create mi_primera_app.py

---
+++ mi_primera_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Título y autor
+st.title("Mi primera app")
+st.write("Esta app fue elaborada por 'Santiago Andrés Mejía García'.")
+
+# Pregunta al usuario por su nombre
+nombre_usuario = st.text_input("Por favor, ingresa tu nombre:")
+
+# Verifica si se ingresó un nombre
+if nombre_usuario:
+    mensaje_bienvenida = f"{nombre_usuario}, te doy la bienvenida a mi primera app."
+    st.write(mensaje_bienvenida)
